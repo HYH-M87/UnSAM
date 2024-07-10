d85cd5beb32fd37ed54c7b0f761accf15592a014
hyh <huangyihong0303@gmail.com>
2024-07-10 14:53:14 +0800

    the script to resize the image in half

---
+++ docs/demos/img_resize.py
@@ -0,0 +1,11 @@
+
+import cv2
+import os
+dire="JPEGImages"
+img_list = os.listdir(dire)
+for i in img_list:
+    
+    
+    img = cv2.imread(os.path.join(dire, i))
+    img = cv2.resize(img,None,fx=0.5,fy=0.5,interpolation=cv2.INTER_CUBIC)
+    cv2.imwrite(os.path.join(dire, 're_'+i),img)
